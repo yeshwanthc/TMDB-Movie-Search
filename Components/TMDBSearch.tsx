156112c61c2be2a71a4f80c1488bf8ef9af04c18
yeshwanth1837 <yeshwanthnaidu62@gmail.com>
2024-12-25 01:35:56 +0530

    max width

--- Components/TMDBSearch.tsx
+++ Components/TMDBSearch.tsx
@@ -50,7 +50,7 @@ export default function TMDBSearch() {
   };
 
   return (
-    <div className="container mx-auto p-4 max-w-xl">
+    <div className="container mx-auto p-4 max-w-2xl">
       <h1 className="text-2xl font-bold mb-4">TMDB Movie Search</h1>
       <SearchForm onSearch={handleSearch} isLoading={loading} />
       {error ? (
