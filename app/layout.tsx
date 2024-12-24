e4593ca386cceff86b748df89cdcb55c0d297839
yeshwanth1837 <yeshwanthnaidu62@gmail.com>
2024-12-25 01:29:54 +0530

    tailwind cdn

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css"
+          rel="stylesheet"
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
