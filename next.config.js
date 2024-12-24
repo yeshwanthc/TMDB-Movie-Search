f1758b523335cd296eea51a597e8e7afb6073d08
yeshwanth1837 <yeshwanthnaidu62@gmail.com>
2024-12-25 00:54:00 +0530

    config file fix

---
+++ next.config.js
@@ -0,0 +1,12 @@
+const path = require("path");
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'export',  
+  images: {
+    unoptimized: true, 
+  },
+};
+
+module.exports = nextConfig;
